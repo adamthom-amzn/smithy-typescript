6a7e0f8fc838794a181a56c514e2eed15f96cf6b
Adam Thomas <adamthom@amazon.com>
2021-05-27 15:57:43 -0700

    Add support for SSDK-default validation

--- smithy-typescript-ssdk-libs/server-common/src/validation/index.ts
+++ smithy-typescript-ssdk-libs/server-common/src/validation/index.ts
@@ -13,6 +13,8 @@
  *  permissions and limitations under the License.
  */
 
+import { SmithyException } from "@aws-sdk/smithy-client";
+
 export * from "./validators";
 
 interface StandardValidationFailure<ConstraintBoundsType, FailureType> {
@@ -46,7 +48,7 @@ export interface RangeValidationFailure extends StandardValidationFailure<number
 
 export class RequiredValidationFailure {
   memberName: string;
-  constraintType = "required";
+  constraintType: "required" = "required";
 
   constructor(memberName: string) {
     this.memberName = memberName;
@@ -66,3 +68,83 @@ export type ValidationFailure =
   | RangeValidationFailure
   | RequiredValidationFailure
   | UniqueItemsValidationFailure;
+
+export interface ValidationContext<O extends string> {
+  operation: O;
+}
+
+export type ValidationCustomizer<O extends string> = (
+  context: ValidationContext<O>,
+  failures: ValidationFailure[]
+) => SmithyException | undefined;
+
+export const generateValidationSummary = (failures: readonly ValidationFailure[]): string => {
+  let failingMembers = new Set(failures.map((failure) => failure.memberName));
+
+  var message = `${failures.length} validation error${failures.length > 1 ? "s" : ""} `;
+
+  if (failures.length > 1) {
+    message += `across ${failingMembers.size} ` + `member${failingMembers.size > 1 ? "s" : ""} `;
+  }
+
+  message += `detected. `;
+
+  message += `Member '${failures[0].memberName}' failed: ${generateValidationMessage(failures[0])}.`;
+
+  return message;
+};
+
+export const generateValidationMessage = (failure: ValidationFailure): string => {
+  let failureValue = failure.constraintType === "required" ? "null" : failure.failureValue.toString();
+
+  let prefix: string;
+  let suffix: string;
+  switch (failure.constraintType) {
+    case "required": {
+      prefix = "Value";
+      suffix = "must not be null";
+      break;
+    }
+    case "enum": {
+      prefix = "Value";
+      suffix = `must satisfy enum value set: ${failure.constraintValues}`;
+      break;
+    }
+    case "length": {
+      prefix = "Value with length";
+      let min = failure.constraintValues[0];
+      let max = failure.constraintValues[1];
+      if (min === undefined) {
+        suffix = `must have length less than or equal to ${max}`;
+      } else if (max === undefined) {
+        suffix = `must have length greater than or equal to ${min}`;
+      } else {
+        suffix = `must have length between ${min} and ${max}, inclusive`;
+      }
+      break;
+    }
+    case "pattern": {
+      prefix = "Value";
+      suffix = `must satisfy regular expression pattern: ${failure.constraintValues}`;
+      break;
+    }
+    case "range": {
+      prefix = "Value";
+      let min = failure.constraintValues[0];
+      let max = failure.constraintValues[1];
+      if (min === undefined) {
+        suffix = `must be less than or equal to ${max}`;
+      } else if (max === undefined) {
+        suffix = `must be greater than or equal to ${min}`;
+      } else {
+        suffix = `must be between ${min} and ${max}, inclusive`;
+      }
+      break;
+    }
+    case "uniqueItems": {
+      prefix = "Value with repeated values";
+      suffix = "must have unique values";
+    }
+  }
+  return `${prefix} ${failureValue} failed to satisfy constraint: Member ${suffix}`;
+};
